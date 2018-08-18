540692a2827b764a5fb06c4f38b744705647c04f
seunghyupoh3517 <seunghyupoh3517@gmail.com>
2018-08-18 15:28:18 -0700

    08/18/2018

---
+++ 01-webotron/webotron/webotron.py
@@ -0,0 +1,30 @@
+import boto3
+import sys
+import click
+
+session = boto3.Session(profile_name='PythonAutomation')
+s3 = session.resource('s3')
+
+@click.group()
+def cli():
+    "Webtron delpoys websites to AWS"
+    pass
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print (bucket)
+## Document string use --help
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List objects in an s3 bucket"
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
+if __name__ == '__main__':
+    #list_buckets()
+    cli()
+## python convention
+## when this file run as script _name_ will be equalized _main_
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ AWS with Python
 ## 01-webtron
 
 Webotron is a script that will sync a local directory to an s3 bucket, and optionally configure Route 53 and cloudfront as well.
+
+
+### Features
+
+Webtron currently has the following features:
+
+- List bucket
+- List contents of a bucket 
